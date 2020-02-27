ada9abbe0dc01801cfdcc5e7790ebaf2b20a93a3
CD Cabrera <ccabrera@redhat.com>
2020-02-27 11:21:17 -0500

    revert t1.js

--- src/t1.js
+++ src/t1.js
@@ -1,5 +1,5 @@
 const helloWorld = value => `Hello world! How are ${value}`;
 
-const loremIpsum = value => `Lorem ipsum! How are ${value} again`;
+const loremIpsum = value => `Lorem ipsum! How are ${value}`;
 
 module.exports = { helloWorld, loremIpsum };
