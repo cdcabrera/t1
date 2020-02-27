84cf1dcbadb78d9f42432ad94232880ff6439263
CD Cabrera <ccabrera@redhat.com>
2020-02-27 11:11:19 -0500

    Update t1.js (#4)

--- src/t1.js
+++ src/t1.js
@@ -1,5 +1,5 @@
 const helloWorld = value => `Hello world! How are ${value}`;
 
-const loremIpsum = value => `Lorem ipsum! How are ${value}`;
+const loremIpsum = value => `Lorem ipsum! How are ${value} again`;
 
 module.exports = { helloWorld, loremIpsum };
