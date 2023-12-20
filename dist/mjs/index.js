56491327192e2491fc421dd4b826d300774195f5
CD Cabrera <cabrera.code@gmail.com>
2023-12-20 10:26:41 -0500

    fix(build): alternative dist, extra func

--- dist/mjs/index.js
+++ dist/mjs/index.js
@@ -1,4 +1,18 @@
 import React from 'react';
-import { components, One, Two, sampleFunction } from '../../src/';
+import { sampleFunction } from '../../src/sampleFunction.js';
+
+const One = () => {
+  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "One lorem ipsum dolor sit"));
+};
+
+const Two = () => {
+  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h2", null, "Two lorem ipsum dolor sit"));
+};
+
+const components = {
+  One,
+  Two
+};
+
 
 export { One, Two, sampleFunction, components as default };
